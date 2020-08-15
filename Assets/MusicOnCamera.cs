244277c74d34bfa4f3c81a3eecb14e6d2ccc092b
phoenixsong327 <360932990@qq.com>
2020-08-16 02:25:31 +0800

    fmod update

---
+++ Assets/MusicOnCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicOnCamera : MonoBehaviour
+{
+    [FMODUnity.EventRef] public string MusicEvent;
+
+    public FMOD.Studio.EventInstance MusicInstance;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        MusicInstance = FMODUnity.RuntimeManager.CreateInstance(MusicEvent);
+        MusicInstance.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
